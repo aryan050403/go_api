605a4d96ade60802aad7c6c25d14cc583cd7e174
Aryan <aryan33446@gmail.com>
2025-02-11 23:20:23 +0530

    Added several functionalities

--- main.go
+++ main.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+type people struct {
+	name    string
+	phNo    int
+	address string
+}
+
+func (p *people) updateName(updatedName string) people {
+	p.name = updatedName
+	return *p
+}
+func (p *people) printInfo() {
+	fmt.Println("Name:", p.name)
+	fmt.Println("Phone Number:", p.phNo)
+	fmt.Println("Address:", p.address)
+}
 func main() {
-	fmt.Println("Hello World")
+	p := people{
+		name:    "ArShar",
+		phNo:    990990990,
+		address: "JK06",
+	}
+	p.updateName("Ryan")
+	p.printInfo()
 }
